9de3687b48c3b1c6d526aecb0d3667a5987f2ba5
owl <75400842+owl-birds@users.noreply.github.com>
2023-03-20 21:25:35 +0700

    tired

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes";
-import "./App.scss";
 
 const App = () => {
   return (
--- src/presentation/pages/home/Home.tsx
+++ src/presentation/pages/home/Home.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import classes from "./Home.module.scss";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={classes.home}>
+      <section>
+        <span>CMSA</span>
+      </section>
+      <section>
+        <span>RCA</span>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
