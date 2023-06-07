ceac9ecfbab3283d4caffa43b96ba58ea1e8d077
owl <75400842+owl-birds@users.noreply.github.com>
2023-06-07 21:41:53 +0700

    input data structure edited

--- src/infrastructure/accepted_input_structure_data.ts
+++ src/infrastructure/accepted_input_structure_data.ts
@@ -1,51 +1,88 @@
 // one ;eve;
 // country
 export const one_level_country_data = [
-  { country: "country_name", "2010": 111, "2011": "..." },
-  { country: "...", "2010": "...", "2011": "..." },
+  { country: "country_name", "2010": 111, "2011": "...", "...": "..." },
+  { country: "...", "2010": "...", "2011": "...", "...": "..." },
 ];
-export const one_level_country_data_columns = ["country", "2010", "2011"];
+export const one_level_country_data_columns = [
+  "country",
+  "2010",
+  "2011",
+  "...",
+];
 // world
-export const one_level_world_data = [{ "2010": 111, "2011": "..." }];
-export const one_level_world_data_columns = ["2010", "2011"];
+export const one_level_world_data = [
+  { "2010": 111, "2011": "...", "...": "..." },
+];
+export const one_level_world_data_columns = ["2010", "2011", "..."];
 
 // two level com
 // country
 export const two_level_com_country_data = [
-  { country: "country_name", commodity: "A", "2010": 111, "2011": "..." },
-  { country: "...", commodity: "...", "2010": "...", "2011": "..." },
+  {
+    country: "country_name",
+    commodity: "A",
+    "2010": 111,
+    "2011": "...",
+    "...": "...",
+  },
+  {
+    country: "...",
+    commodity: "...",
+    "2010": "...",
+    "2011": "...",
+    "...": "...",
+  },
 ];
 export const two_level_com_country_data_columns = [
   "country",
   "commodity",
   "2010",
   "2011",
+  "...",
 ];
 // world
 export const two_level_com_world_data = [
-  { commodity: "A", "2010": 111, "2011": "..." },
-  { commodity: "...", "2010": "...", "2011": "..." },
+  { commodity: "A", "2010": 111, "2011": "...", "...": "..." },
+  { commodity: "...", "2010": "...", "2011": "...", "...": "..." },
 ];
-export const two_level_com_world_data_columns = ["commodity", "2010", "2011"];
+export const two_level_com_world_data_columns = [
+  "commodity",
+  "2010",
+  "2011",
+  "...",
+];
 
 // two level reg/partner
 // country
 export const two_level_reg_country_data = [
-  { country: "country_name", region: "A", "2010": 111, "2011": "..." },
-  { country: "...", region: "...", "2010": "...", "2011": "..." },
+  {
+    country: "country_name",
+    region: "A",
+    "2010": 111,
+    "2011": "...",
+    "...": "...",
+  },
+  { country: "...", region: "...", "2010": "...", "2011": "...", "...": "..." },
 ];
 export const two_level_reg_country_data_columns = [
   "country",
   "region",
   "2010",
   "2011",
+  "...",
 ];
 // world
 export const two_level_reg_world_data = [
-  { region: "A", "2010": 111, "2011": "..." },
-  { region: "...", "2010": "...", "2011": "..." },
+  { region: "A", "2010": 111, "2011": "...", "...": "..." },
+  { region: "...", "2010": "...", "2011": "...", "...": "..." },
 ];
-export const two_level_reg_world_data_columns = ["region", "2010", "2011"];
+export const two_level_reg_world_data_columns = [
+  "region",
+  "2010",
+  "2011",
+  "...",
+];
 
 // three level
 // country
@@ -56,6 +93,7 @@ export const three_level_country_data = [
     region: "B",
     "2010": 111,
     "2011": "...",
+    "...": "...",
   },
   {
     country: "...",
@@ -63,6 +101,7 @@ export const three_level_country_data = [
     region: "...",
     "2010": "...",
     "2011": "...",
+    "...": "...",
   },
 ];
 export const three_level_country_data_columns = [
@@ -71,13 +110,19 @@ export const three_level_country_data_columns = [
   "region",
   "2010",
   "2011",
+  "...",
 ];
 // world
 export const three_level_world_data = [
-  { commodity: "A", "2010": 111, "2011": "..." },
-  { commodity: "...", "2010": "...", "2011": "..." },
+  { commodity: "A", "2010": 111, "2011": "...", "...": "..." },
+  { commodity: "...", "2010": "...", "2011": "...", "...": "..." },
 ];
-export const three_level_world_data_columns = ["commodity", "2010", "2011"];
+export const three_level_world_data_columns = [
+  "commodity",
+  "2010",
+  "2011",
+  "...",
+];
 
 // RCA basic
 // can use three level or two level commodity
